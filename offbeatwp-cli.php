76312e3054b7370179157929b5e77ec48434b4ce
Maikel Haarmans <maikel@raow.nl>
2024-01-03 15:51:15 +0100

    trigger errors with caught exceptions

--- offbeatwp-cli.php
+++ offbeatwp-cli.php
@@ -2,16 +2,16 @@
 $composerAutoload = __DIR__ . '/vender/autoload.php';
 
 if (is_dir($composerAutoload)) {
-	require_once $composerAutoload;
+    require_once $composerAutoload;
 }
 
 if (!class_exists('WP_CLI')) {
-	trigger_error('Offbeatwp-cli cannot work without WP_CLI.');
-	return;
+    trigger_error('Offbeatwp-cli cannot work without WP_CLI.');
+    return;
 }
 
 try {
-	WP_CLI::add_command('offbeatwp', OffbeatCLI\OffbeatCommands::class);
+    WP_CLI::add_command('offbeatwp', OffbeatCLI\OffbeatCommands::class);
 } catch (Exception $e) {
-
+    trigger_error($e->getMessage());
 }
