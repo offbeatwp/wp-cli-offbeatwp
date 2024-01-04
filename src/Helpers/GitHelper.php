15a2b1efca5027809b2aa548d4aabe33df2d25d9
Maikel Haarmans <maikel@raow.nl>
2024-01-04 15:26:11 +0100

    Remove leftover files after pull

--- src/Helpers/GitHelper.php
+++ src/Helpers/GitHelper.php
@@ -8,16 +8,17 @@ final class GitHelper
 {
     public static function fetch(string $name): void
     {
+        // Pull from git
         $repositoryUrl = 'http://git.raow.work:88/raow/offbeat-base-module-repo.git';
-        $targetDir = getcwd() . '/temp';
+        $tempDir = getcwd() . '/temp';
 
-        exec("git clone --depth 1 --no-checkout {$repositoryUrl} {$targetDir}");
+        exec("git clone --depth 1 --no-checkout {$repositoryUrl} {$tempDir}");
 
-        if (!is_dir($targetDir) && !mkdir($targetDir) && !is_dir($targetDir)) {
-            throw new RuntimeException(sprintf('Directory "%s" was not created', $targetDir));
+        if (!is_dir($tempDir) && !mkdir($tempDir) && !is_dir($tempDir)) {
+            throw new RuntimeException(sprintf('Directory "%s" was not created', $tempDir));
         }
 
-        chdir($targetDir);
+        chdir($tempDir);
 
         exec('git config core.sparseCheckout true');
 
@@ -25,5 +26,11 @@ final class GitHelper
 
         exec('git read-tree -mu HEAD');
         exec('git pull origin main');
+
+        // Move from temp to src
+        rename($tempDir . '/' . $name, getcwd() . '/src');
+
+        // Delete leftovers
+        rmdir($tempDir);
     }
 }
